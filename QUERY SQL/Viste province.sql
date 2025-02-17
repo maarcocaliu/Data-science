23113fda3314bf95854b173e5a110e1423000e48
maarcocaliu <caliumimarco@outlook.com>
2025-02-17 20:58:42 +0100

    Update Viste province.sql

--- QUERY SQL/Viste province.sql
+++ QUERY SQL/Viste province.sql
@@ -1,82 +1,50 @@
 CREATE VIEW bo_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'BO'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'BO'
+AND giorno <= '2019-12-24';
 
 CREATE VIEW mo_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'MO'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'MO'
+AND giorno <= '2019-12-24';
 
 CREATE VIEW re_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'RE'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'RE'
+AND giorno <= '2019-12-24';
 
 CREATE VIEW pr_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'PR'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'PR'
+AND giorno <= '2019-12-24';
 
 CREATE VIEW pc_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'PC'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'PC'
+AND giorno <= '2019-12-24';
 
 CREATE VIEW fe_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'FE'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'FE'
+AND giorno <= '2019-12-24';
 
 CREATE VIEW rn_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'RN'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'RN'
+AND giorno <= '2019-12-24';
 
 CREATE VIEW fc_view AS
 SELECT *
 FROM mts_clean
-WHERE postazione IN (
-    SELECT postazione 
-    FROM postazioni 
-    WHERE provincia = 'FC'
-)
-AND data <= '2019-12-24';
+WHERE provincia = 'FC'
+AND giorno <= '2019-12-24';
 
 
 
